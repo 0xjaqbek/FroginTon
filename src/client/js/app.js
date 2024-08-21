2831bdc884d432fb9daedea97d5e1e1d10c69457
0xjaqbek <jaqbek.eth@gmail.com>
2024-08-21 02:25:01 +0200

    bg color

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -55,6 +55,7 @@ window.onload = function () {
     if (isTelegramWebApp) {
         console.log("Running in Telegram Web App");
         window.Telegram.WebApp.disableVerticalSwipes();
+        Telegram.WebApp.header_bg_color
 
         Telegram.WebApp.ready();
 
