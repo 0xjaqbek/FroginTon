6ae9caa164493928fac056403ccc9e5d8a3a5ade
0xjaqbek <jaqbek.eth@gmail.com>
2024-08-23 12:53:44 +0200

    ,

--- src/server/server.js
+++ src/server/server.js
@@ -35,10 +35,10 @@ const admin = require('firebase-admin');
 
 const serviceAccount = {
   type: 'service_account',
-  project_id: "frogin-485a6",
+  project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Ensure newlines are handled correctly
-  client_email: "firebase-adminsdk-k3ymh@frogin-485a6.iam.gserviceaccount.com",
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
   token_uri: process.env.FIREBASE_TOKEN_URI,
