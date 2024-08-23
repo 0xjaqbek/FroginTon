09c74b1654538f2c946fe9d0a3b042a7febb37d9
0xjaqbek <jaqbek.eth@gmail.com>
2024-08-23 12:47:40 +0200

    ..

--- src/server/server.js
+++ src/server/server.js
@@ -38,7 +38,7 @@ const serviceAccount = {
   project_id: "frogin-485a6",
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Ensure newlines are handled correctly
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_email: "firebase-adminsdk-k3ymh@frogin-485a6.iam.gserviceaccount.com",
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
   token_uri: process.env.FIREBASE_TOKEN_URI,
