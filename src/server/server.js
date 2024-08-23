90a1d48fc9fe65e06549dd900ae4473f35c8b7a4
0xjaqbek <jaqbek.eth@gmail.com>
2024-08-23 04:13:15 +0200

    update firebase+env

--- src/server/server.js
+++ src/server/server.js
@@ -28,13 +28,26 @@ const Vector = SAT.Vector;
 
 app.use(express.static(__dirname + '/../client'));
 
-// Firebase initialization
+require('dotenv').config();  // Add this line at the beginning
+
 const admin = require('firebase-admin');
-const serviceAccount = require('../client/frogin-485a6-firebase-adminsdk-k3ymh-4849dec6d3.json');
+
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Ensure newlines are handled correctly
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://frogin-485a6-default-rtdb.europe-west1.firebasedatabase.app/'
+  databaseURL: 'https://your-database-url.firebaseio.com'
 });
 
 const db = admin.database();
